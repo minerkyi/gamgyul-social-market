a24a66999841615972d465e5058e1c95390ec090
김용일 <minerkyi@gmail.com>
2025-06-23 14:12:44 +0900

    Header 컴포넌트 속성 값 추가

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import Header from '../../components/Header'
 
 export default function Products() {
+
+  const handleSave = () => {
+    alert('이벤트 동작');
+  };
+
   return (
     <>
-      <Header title="상품 목록" />
+      <Header title="상품 목록" type="profile" onClick={handleSave} />
       <section>
         <h2>Products</h2>
         <p>상품 목록</p>
