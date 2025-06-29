8efa94327ae3674e282b2d89670b19e3f427303f
winterrbell <edong1291@gmail.com>
2025-06-30 00:13:51 +0900

    feat: userContext 작성

--- src/App.jsx
+++ src/App.jsx
@@ -12,32 +12,45 @@ import ProfilePage from './pages/Profile/ProfilePage';
 import Footer from './components/Footer';
 import Page404 from './pages/Page404';
 
+import LoginMain from './pages/login/LoginMain';
+import EmailLoginPage from './pages/login/EmailLoginPage'; 
+import SignupPage from './pages/login/SignupPage';
+import SignupProfilePage from './pages/login/SignupProfilePage';
+import { UserProvider } from './contexts/UserProvider';
+
 function App() {
   return (
-    <BrowserRouter basename="/gamgyul-social-market">
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/sample" element={<Sample />} />
+    <UserProvider>
+      <BrowserRouter basename="/gamgyul-social-market">
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/sample" element={<Sample />} />
 
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/profile/edit" element={<ProfileEditPage />} />
-        <Route path="/profile/:accountname" element={<ProfilePage />} />
-        <Route
-          path="/profile/:accountname/followers"
-          element={<FollowListPage />}
-        />
-        <Route
-          path="/profile/:accountname/followings"
-          element={<FollowListPage />}
-        />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/profile/edit" element={<ProfileEditPage />} />
+          <Route path="/profile/:accountname" element={<ProfilePage />} />
+          <Route
+            path="/profile/:accountname/followers"
+            element={<FollowListPage />}
+          />
+          <Route
+            path="/profile/:accountname/followings"
+            element={<FollowListPage />}
+          />
 
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-      <FooterProvider>
-        <Footer />
-      </FooterProvider>
-    </BrowserRouter>
+          <Route path="*" element={<Page404 />} />
+
+          <Route path='/login/signup' element={<SignupPage />} />
+          <Route path='/login/signup/profile' element={<SignupProfilePage />} />
+          <Route path='/login' element={<LoginMain />} />
+          <Route path='/login/email' element={<EmailLoginPage />} />
+        </Routes>
+        <FooterProvider>
+          <Footer />
+        </FooterProvider>
+      </BrowserRouter>
+    </UserProvider>
   );
 }
 
