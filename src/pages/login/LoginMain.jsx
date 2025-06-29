0cbbf1df6694954c296a38a4b6e42ea2b40520a7
winterrbell <edong1291@gmail.com>
2025-06-30 00:43:15 +0900

    feat: 로그인 메인페이지 마크업
    #20

---
+++ src/pages/login/LoginMain.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./LoginMain.module.css";
+import symbolLogoW from "../../assets/symbol-logo-W.png";
+import iconKakao from "../../assets/message-circle.png";
+import iconGoogle from "../../assets/google.png";
+import iconFacebook from "../../assets/facebook.png";
+import SnsButton from "../../components/common/Button/SnsButton";
+
+function LoginMain() {
+  const navigate = useNavigate();
+
+  const goToEmailLoginPage = () => {
+    navigate("/login/email");
+  };
+
+  const goToSignupPage = () => {
+    navigate("/login/signup");
+  };
+
+  return (
+    <div className={styles["login-main"]}>
+      <img className={styles.logo} src={symbolLogoW} alt="로고" />
+      <div className={styles.container}>
+        <SnsButton
+          icon={iconKakao}
+          text="카카오톡 계정으로 로그인"
+          type="kakao"
+        />
+        <SnsButton
+          icon={iconGoogle}
+          text="구글 계정으로 로그인"
+          type="google"
+        />
+        <SnsButton
+          icon={iconFacebook}
+          text="페이스북 계정으로 로그인"
+          type="facebook"
+        />
+        <div className={styles["email-login"]}>
+          <button
+            className={styles["email-btn"]}
+            type="button"
+            onClick={goToEmailLoginPage}
+          >
+            이메일로 로그인
+          </button>
+          <button
+            className={styles["signup-btn"]}
+            type="button"
+            onClick={goToSignupPage}
+          >
+            회원가입
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginMain;
