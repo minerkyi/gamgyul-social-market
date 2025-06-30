2bee94c30ce0c9aa3928e2fa6c80ad94e7e20438
winterrbell <edong1291@gmail.com>
2025-06-30 13:27:31 +0900

    style: 메인 페이지 배경 적용 및 레이아웃 개선

--- src/pages/login/LoginMain.jsx
+++ src/pages/login/LoginMain.jsx
@@ -19,39 +19,43 @@ function LoginMain() {
   };
 
   return (
-    <div className={styles["login-main"]}>
-      <img className={styles.logo} src={symbolLogoW} alt="로고" />
+    <div className={styles.background}>
       <div className={styles.container}>
-        <SnsButton
-          icon={iconKakao}
-          text="카카오톡 계정으로 로그인"
-          type="kakao"
-        />
-        <SnsButton
-          icon={iconGoogle}
-          text="구글 계정으로 로그인"
-          type="google"
-        />
-        <SnsButton
-          icon={iconFacebook}
-          text="페이스북 계정으로 로그인"
-          type="facebook"
-        />
-        <div className={styles["email-login"]}>
-          <button
-            className={styles["email-btn"]}
-            type="button"
-            onClick={goToEmailLoginPage}
-          >
-            이메일로 로그인
-          </button>
-          <button
-            className={styles["signup-btn"]}
-            type="button"
-            onClick={goToSignupPage}
-          >
-            회원가입
-          </button>
+        <div className={styles["login-main"]}>
+          <img className={styles.logo} src={symbolLogoW} alt="로고" />
+          <div className={styles.snscontainer}>
+            <SnsButton
+              icon={iconKakao}
+              text="카카오톡 계정으로 로그인"
+              type="kakao"
+            />
+            <SnsButton
+              icon={iconGoogle}
+              text="구글 계정으로 로그인"
+              type="google"
+            />
+            <SnsButton
+              icon={iconFacebook}
+              text="페이스북 계정으로 로그인"
+              type="facebook"
+            />
+            <div className={styles["email-login"]}>
+              <button
+                className={styles["email-btn"]}
+                type="button"
+                onClick={goToEmailLoginPage}
+              >
+                이메일로 로그인
+              </button>
+              <button
+                className={styles["signup-btn"]}
+                type="button"
+                onClick={goToSignupPage}
+              >
+                회원가입
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
